c8997efda1d77b5b5143eccd891b305bcf6f81ce
JakeJ16c <jakejones16c@gmail.com>
2025-05-25 19:20:29 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,11 @@
+<!-- firebase.js -->
 <script type="module">
+  // Import Firebase SDK modules from CDN
   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
   import { getAuth } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-auth.js";
   import { getFirestore } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-firestore.js";
 
+  // Your Firebase configuration
   const firebaseConfig = {
     apiKey: "AIzaSyA6kN9-7dN9Ovq6BmWBBJwBhLXRW6INX4c",
     authDomain: "daisy-s-website.firebaseapp.com",
@@ -13,10 +16,12 @@
     measurementId: "G-ST5CQ6PV41"
   };
 
+  // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
   const db = getFirestore(app);
 
+  // Make Firebase available to other scripts globally
   window.firebaseApp = app;
   window.firebaseAuth = auth;
   window.firebaseDB = db;
