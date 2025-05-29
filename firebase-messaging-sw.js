befac54d14ccb5a58bbb04d7075cad433ad288e1
JakeJ16c <jakejones16c@gmail.com>
2025-05-29 10:38:37 +0100

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,22 @@
+importScripts("https://www.gstatic.com/firebasejs/11.8.1/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/11.8.1/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyA6K...", // same as above
+  projectId: "daisy-s-website",
+  messagingSenderId: "595443495060",
+  appId: "1:595443495060:web:...your id"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log("[firebase-messaging-sw.js] Received background message ", payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: "/icon-192.png"
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
