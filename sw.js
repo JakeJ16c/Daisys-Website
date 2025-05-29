864b309b991849fbc70e763754a31442b856e24c
JakeJ16c <jakejones16c@gmail.com>
2025-05-29 08:40:25 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,23 @@
 
 const CACHE_NAME = "golden-cache-v1";
 const urlsToCache = [
-  "/Daisys-Website-main/",
-  "/Daisys-Website-main/index.html",
-  "/Daisys-Website-main/styles.css",
-  "/Daisys-Website-main/firebase.js",
-  "/Daisys-Website-main/basket.js",
-  "/Daisys-Website-main/favicon_circle.ico"
+  "./",
+  "./index.html",
+  "./shop.html",
+  "./account.html",
+  "./checkout.html",
+  "./styles.css",
+  "./firebase.js",
+  "./basket.js",
+  "./account.js",
+  "./checkout.js",
+  "./favicon_circle.ico",
+  "./IMG_5319.jpg",
+  "./image0.jpeg",
+  "./image1.jpeg",
+  "./image2.jpeg",
+  "./image3.jpeg",
+  "./image4.jpeg"
 ];
 
 self.addEventListener("install", event => {
