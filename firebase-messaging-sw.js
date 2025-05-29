18acc69da6fc8d93444b9dbbd9eb32add7ccf8a5
JakeJ16c <jakejones16c@gmail.com>
2025-05-29 10:39:42 +0100

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -5,7 +5,7 @@ firebase.initializeApp({
   apiKey: "AIzaSyA6kN9-7dN9Ovq6BmWBBJwBhLXRW6INX4c", // same as above
   projectId: "daisy-s-website",
   messagingSenderId: "595443495060",
-  appId: "1:595443495060:web:...your id"
+  appId: "1:595443495060:web:7bbdd1108ad336d55c8481"
 });
 
 const messaging = firebase.messaging();
@@ -15,7 +15,7 @@ messaging.onBackgroundMessage(function(payload) {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: "/icon-192.png"
+    icon: "icon-192.png"
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
