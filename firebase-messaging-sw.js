de8d7f2f5543edc532c25cf41756597db77d6537
JakeJ16c <jakejones16c@gmail.com>
2025-05-31 01:15:58 +0100

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,9 +1,12 @@
-importScripts("https://www.gstatic.com/firebasejs/11.8.1/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/11.8.1/firebase-messaging-compat.js");
+// firebase-messaging-sw.js
+importScripts('https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/11.8.1/firebase-messaging.js');
 
 firebase.initializeApp({
-  apiKey: "AIzaSyA6kN9-7dN9Ovq6BmWBBJwBhLXRW6INX4c", 
+  apiKey: "AIzaSyA6kN9-7dN9Ovq6BmWBBJwBhLXRW6INX4c",
+  authDomain: "daisy-s-website.firebaseapp.com",
   projectId: "daisy-s-website",
+  storageBucket: "daisy-s-website.firebasestorage.app",
   messagingSenderId: "595443495060",
   appId: "1:595443495060:web:7bbdd1108ad336d55c8481"
 });
@@ -11,11 +14,12 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function(payload) {
-  console.log("[firebase-messaging-sw.js] Received background message ", payload);
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: "icon-192.png"
+    icon: '/icon-192.png' // Or another icon in your public assets
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
