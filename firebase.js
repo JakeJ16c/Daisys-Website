2b5c53e0757f7dd47c44884c23e13ef148cfc6fe
JakeJ16c <jakejones16c@gmail.com>
2025-06-01 13:18:10 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-storage.js"; // 
 import { getMessaging, getToken, onMessage } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-messaging.js";
 
 // Your Firebase config (real values from your console)
@@ -18,7 +19,8 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
 const messaging = getMessaging(app);
 
 // Export instances to use elsewhere
-export { app, auth, db, messaging };
+export { app, auth, db, storage, messaging };
