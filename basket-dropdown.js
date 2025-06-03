f2c771b21fa0757a76a00315c3f4a8bf740a82b1
JakeJ16c <jakejones16c@gmail.com>
2025-06-03 20:00:03 +0100

    Update basket-dropdown.js

--- basket-dropdown.js
+++ basket-dropdown.js
@@ -160,6 +160,9 @@ document.addEventListener("DOMContentLoaded", () => {
     subtotalEl.style.textAlign = "right";
     subtotalEl.style.fontWeight = "bold";
     subtotalEl.style.margin = "0.5rem 0 1rem 0";
+        // Divider
+          const hr = document.createElement("hr");
+          hr.style.margin = "0.5rem 0"; 
     basketPreview.appendChild(hr);
     basketPreview.appendChild(subtotalEl);
 
