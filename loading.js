26e1cfac94c1fe982a7c17cf47dc3b52412b8fc2
JakeJ16c <jakejones16c@gmail.com>
2025-07-10 21:49:43 +0100

    Update loading.js

--- loading.js
+++ loading.js
@@ -102,7 +102,7 @@ window.addEventListener("load", () => {
     
     document.body.style.opacity = "0";
     setTimeout(() => {
-      document.body.style.transition = "opacity 0.6s ease";
+      document.body.style.transition = "opacity 3s ease";
       document.body.style.opacity = "1";
     }, 0);
 
