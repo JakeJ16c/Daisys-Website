3188b02bbcafd309da2cf959b22cb05de9445a4c
JakeJ16c <jakejones16c@gmail.com>
2025-07-13 23:15:25 +0100

    Create analytics.js

---
+++ analytics.js
@@ -0,0 +1,8 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag('js', new Date());
+
+// Replace with your actual G-code
+gtag('config', 'G-ST5CQ6PV41');
