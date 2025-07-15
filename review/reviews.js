c93aa65458ce116acf46172c5e29df83a05a4c7c
JakeJ16c <jakejones16c@gmail.com>
2025-07-15 21:00:22 +0100

    Update reviews.js

--- review/reviews.js
+++ review/reviews.js
@@ -18,8 +18,8 @@ async function loadProductInfo() {
     const productData = productSnap.exists() ? productSnap.data() : {};
 
     const name = productData.name || "Unnamed Product";
-    const image = Array.isArray(data.images) && data.images.length > 0
-      ? data.images[0]
+    const image = Array.isArray(productData.images) && productData.images.length > 0
+      ? productData.images[0]
       : "favicon_circle.ico";
 
     // Inject into DOM
